f2fefe2ce4ec8ec6a8d9b90b8c98a8590d416b8c
Ulrich Buchgraber <ulrich.b@gmx.at>
2015-02-25 22:47:55 +0100

    Extracted MissingXmlDocHighlightingTestsBase class, added new DefaultSettingsTests, fixed non side-effect free changing of settings (in WithProject) with ExecuteWithinSettingsTransaction(), moved .gold files from sample project to the test project (next to the test fixtures)

---
+++ XmlDocInspections.Plugin.Tests/IntegrationTests/MissingXmlDocHighlightingTestsBase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using JetBrains.Annotations;
+using JetBrains.Application.Settings;
+using JetBrains.ProjectModel;
+using JetBrains.ProjectModel.DataContext;
+using JetBrains.ReSharper.Daemon;
+using JetBrains.ReSharper.Daemon.CSharp;
+using XmlDocInspections.Plugin.Highlighting;
+
+namespace XmlDocInspections.Plugin.Tests.IntegrationTests
+{
+    public abstract class MissingXmlDocHighlightingTestsBase : CSharpHighlightingTestNet4Base
+    {
+        protected override string RelativeTestDataPath
+        {
+            get { return "."; }
+        }
+
+        protected override string GetGoldTestDataPath(string fileName)
+        {
+            return Path.Combine(GetCodeFileDirectoryPath(), GetType().Name, fileName + ".gold");
+        }
+
+        private static string GetCodeFileDirectoryPath([CallerFilePath] string callerFilePath = null)
+        {
+            Trace.Assert(callerFilePath != null);
+            return Path.GetDirectoryName(callerFilePath);
+        }
+
+        protected override bool HighlightingPredicate(IHighlighting highlighting, IContextBoundSettingsStore settingsStore)
+        {
+            return highlighting is XmlDocHighlightingBase;
+        }
+
+        protected override void DoNamedTest2(params string[] auxFiles)
+        {
+            ExecuteWithinSettingsTransaction(settingsStore =>
+            {
+                RunGuarded(() => MutateSettings(settingsStore));
+                base.DoNamedTest2(auxFiles);
+            });
+        }
+
+        protected abstract void MutateSettings([NotNull]IContextBoundSettingsStore settingsStore);
+    }
+}
