08928f6a11802d7cc0208a3dd8c2637f2a7b6396
Ulrich Buchgraber <ulrich.b@gmx.at>
2016-07-15 20:51:10 +0200

    Replace CSharpHighlightingTestBase (will be removed in 2016.2 SDK) usage with TestNetFramework4-attribute

--- Src/XmlDocInspections.Plugin.Tests/Integrative/MissingXmlDocHighlightingTestsBase.cs
+++ Src/XmlDocInspections.Plugin.Tests/Integrative/MissingXmlDocHighlightingTestsBase.cs
@@ -4,11 +4,13 @@ using JetBrains.Application.Settings;
 using JetBrains.ReSharper.Feature.Services.Daemon;
 using JetBrains.ReSharper.FeaturesTestFramework.Daemon;
 using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.TestFramework;
 using XmlDocInspections.Plugin.Highlighting;
 
 namespace XmlDocInspections.Plugin.Tests.Integrative
 {
-    public abstract class MissingXmlDocHighlightingTestsBase : CSharpHighlightingTestNet4Base
+    [TestNetFramework4]
+    public abstract class MissingXmlDocHighlightingTestsBase : CSharpHighlightingTestBase
     {
         protected override string RelativeTestDataPath => "Highlighting";
 
