e9c9b4d39d900f3d08e0ae0aabcd7b7904f14c26
Matthias Koch <ithrowexceptions@gmail.com>
2020-11-20 19:52:56 +0100

    Update Build.ps1

--- Build/Build.ps1
+++ Build/Build.ps1
@@ -15,7 +15,7 @@ trap { $error[0] | Format-List -Force; $host.SetShouldExit(1) }
 
 $BuildOutputPath = "Build\Output"
 $SolutionFilePath = "XmlDocInspections.sln"
-$MSBuildPath = (Get-ChildItem "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2017\*\MSBuild\15.0\Bin\MSBuild.exe").FullName
+$MSBuildPath = (Get-ChildItem "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2019\*\MSBuild\Current\Bin\MSBuild.exe").FullName
 $NUnitAdditionalArgs = "--x86 --labels=All --agents=1"
 $NUnitTestAssemblyPaths = @(
     "Src\XmlDocInspections.Plugin.Tests\bin\RD20202\$Configuration\XmlDocInspections.Plugin.Tests.RD20202.dll"
