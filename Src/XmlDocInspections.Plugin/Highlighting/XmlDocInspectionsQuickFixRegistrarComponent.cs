ef60fd5fb1a2cdbed205de577a91d7ba2640b32d
Ulrich Buchgraber <ulrich.b@gmx.at>
2024-10-29 16:23:10 +0100

    Add ReSharper and Rider 2024.2 support

--- Src/XmlDocInspections.Plugin/Highlighting/XmlDocInspectionsQuickFixRegistrarComponent.cs
+++ Src/XmlDocInspections.Plugin/Highlighting/XmlDocInspectionsQuickFixRegistrarComponent.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using JetBrains.Application;
+using JetBrains.Application.Parts;
 using JetBrains.Lifetimes;
 using JetBrains.ReSharper.Feature.Services.QuickFixes;
 using JetBrains.ReSharper.Intentions.CSharp.QuickFixes;
@@ -8,7 +9,7 @@ using JetBrains.ReSharper.Psi.CSharp.Tree;
 
 namespace XmlDocInspections.Plugin.Highlighting
 {
-    [ShellComponent]
+    [ShellComponent(Instantiation.DemandAnyThreadSafe)]
     internal class XmlDocInspectionsQuickFixRegistrarComponent : IQuickFixesProvider
     {
         public IEnumerable<Type> Dependencies => Array.Empty<Type>();
